a3b25c084e512eebcf8a3f209c4f8e0081db053d
yuuaguiar <yuri_aguiar@live.com>
2025-09-15 20:59:13 -0300

    Recomeco

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Projeto_Integrador_Error404 project.
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto_Integrador_Error404.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
--- gestao/apps.py
+++ gestao/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class YuriAguiarConfig(AppConfig):
+class GestaoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'yuri_aguiar'
+    name = 'gestao'
